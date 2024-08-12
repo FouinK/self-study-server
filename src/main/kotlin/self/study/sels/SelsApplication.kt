2a6ec13cdfec667d956edf9a7d85893cea185112
Roy <roy@cleaninglab.co.kr>
2024-08-12 22:11:40 +0900

    first commit

---
+++ src/main/kotlin/self/study/sels/SelsApplication.kt
@@ -0,0 +1,11 @@
+package self.study.sels
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SelsApplication>(*args)
+}
---
+++ src/test/kotlin/self/study/sels/SelsApplicationTests.kt
@@ -0,0 +1,13 @@
+package self.study.sels
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

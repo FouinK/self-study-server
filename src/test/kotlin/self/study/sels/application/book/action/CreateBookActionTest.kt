79f87f8f7c4fa8674a424cce5fb5cc35cbeb19d9
Roy <yyss9103@gmail.com>
2024-08-18 12:46:06 +0900

    book 만드는 테스트 케이스 구현

---
+++ src/test/kotlin/self/study/sels/application/book/action/CreateBookActionTest.kt
@@ -0,0 +1,68 @@
+package self.study.sels.application.book.action
+
+import fixtures.BookcaseBuilder
+import fixtures.MemberBuilder
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import self.study.sels.application.book.port.`in`.CreateBookCommand
+import self.study.sels.application.book.port.`in`.CreateBookUseCase
+import self.study.sels.model.book.BookRepository
+import self.study.sels.model.book_case.Bookcase
+import self.study.sels.model.book_case.BookcaseRepository
+import self.study.sels.model.member.Member
+import self.study.sels.model.member.MemberRepository
+
+@SpringBootTest
+class CreateBookActionTest(
+    @Autowired val bookcaseRepository: BookcaseRepository,
+    @Autowired val bookRepository: BookRepository,
+    @Autowired val memberRepository: MemberRepository,
+) {
+    lateinit var createBookUseCase: CreateBookUseCase
+    lateinit var member: Member
+    lateinit var bookcase: Bookcase
+
+    @BeforeEach
+    fun setUp() {
+        createBookUseCase =
+            CreateBookAction(
+                bookcaseRepository,
+                bookRepository,
+            )
+
+        member = memberRepository.save(MemberBuilder().build())
+        bookcase =
+            bookcaseRepository.save(
+                BookcaseBuilder(
+                    name = "영어",
+                    memberId = member.id,
+                ).build(),
+            )
+    }
+
+    @Test
+    fun `book이 정상 저장된다`() {
+        // given
+        val command =
+            CreateBookCommand(
+                bookcaseId = bookcase.id,
+                name = "1단원",
+                memberId = member.id,
+            )
+
+        // when
+        val bookId = createBookUseCase.create(command)
+
+        // then
+        val book =
+            bookRepository.findById(bookId)
+                .orElseThrow { throw Exception("없는 bookId로 조회 테스트 실패") }
+
+        assertThat(book.bookcaseId).isEqualTo(command.bookcaseId)
+        assertThat(book.name).isEqualTo(command.name)
+        assertThat(book.memberId).isEqualTo(command.memberId)
+    }
+}

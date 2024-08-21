963453eacc58ce940fac51c2740042918f311b30
Roy <yyss9103@gmail.com>
2024-08-21 18:48:19 +0900

    Answer 엔티티 매핑

---
+++ src/main/kotlin/self/study/sels/model/answer/Answer.kt
@@ -0,0 +1,29 @@
+package self.study.sels.model.answer
+
+import jakarta.persistence.*
+import self.study.sels.model.BaseTimeEntity
+
+@Entity
+@Table(name = "answer")
+class Answer(
+    questionId: Int,
+    answer: String,
+    correctYn: Boolean,
+) : BaseTimeEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Int = 0
+
+    @Column(name = "question_id", nullable = false)
+    var questionId = questionId
+        protected set
+
+    @Column(name = "answer", nullable = false, length = 255)
+    var answer = answer
+        protected set
+
+    @Column(name = "correct_yn", nullable = false)
+    var correctYn = correctYn
+        protected set
+}
---
+++ src/main/kotlin/self/study/sels/model/answer/AnswerRepository.kt
@@ -0,0 +1,5 @@
+package self.study.sels.model.answer
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AnswerRepository : JpaRepository<Answer, Int>

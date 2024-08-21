8821bd1c3b2486d3bf4f8207b2aa8fd68a582bcf
Roy <yyss9103@gmail.com>
2024-08-21 18:51:15 +0900

    answer, question controller 추가

---
+++ src/main/kotlin/self/study/sels/controller/QuestionController.kt
@@ -0,0 +1,8 @@
+package self.study.sels.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/sels/api/u/question")
+class QuestionController

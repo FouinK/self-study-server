d142d15a5a232a202cd3157713bbe651ecde513f
Roy <yyss9103@gmail.com>
2024-08-24 23:18:39 +0900

    Exception -> NotFoundException 변경

--- src/main/kotlin/self/study/sels/application/question/action/CreateQuestionAnswerAction.kt
+++ src/main/kotlin/self/study/sels/application/question/action/CreateQuestionAnswerAction.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import self.study.sels.application.question.port.`in`.CreateQuestionAnswerCommand
 import self.study.sels.application.question.port.`in`.CreateQuestionAnswerUseCase
+import self.study.sels.exception.NotFoundException
 import self.study.sels.model.book.BookRepository
 import self.study.sels.model.question.Question
 import self.study.sels.model.question.QuestionRepository
@@ -19,7 +20,7 @@ class CreateQuestionAnswerAction(
             questionRepository.save(
                 Question(
                     memberId = command.memberId,
-                    book = bookRepository.findById(command.bookId).orElseThrow { throw Exception("책이 없습니다.") },
+                    book = bookRepository.findById(command.bookId).orElseThrow { throw NotFoundException("책이 없습니다.") },
                     question = command.question,
                     multipleChoiceYn = command.multipleChoiceYn,
                 ),

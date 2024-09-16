4f49ac92d2bc983eac6fd5f8f002facd8600b58b
Roy <yyss9103@gmail.com>
2024-09-16 20:34:43 +0900

    코루틴 job.cancel(), job.join()

--- src/test/kotlin/self/study/sels/SelsApplicationTests.kt
+++ src/test/kotlin/self/study/sels/SelsApplicationTests.kt
@@ -1,8 +1,6 @@
 package self.study.sels
 
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -15,12 +13,39 @@ class SelsApplicationTests {
     }
 
     fun main(): Unit = runBlocking {
-        println("start")
-        launch {
-            newRoutine()
+        val job1 = launch {
+            delay(1000)
+            printWithThread("job 1")
         }
-        yield()
-        println("end")
+
+        job1.join() // 없으면 중첩돼서 대기하고 있다면 직관적 코드 진행처럼 1초 대기 후에 아래 코루틴이 실행됨
+
+        val job2 = launch {
+            delay(1000)
+            printWithThread("job 2")
+        }
+    }
+
+    fun example3(): Unit = runBlocking {
+        val job = launch {
+            (1..5).forEach {
+                printWithThread(it)
+                delay(500)
+            }
+        }
+
+        delay(1_000L)
+        job.cancel()
+    }
+
+    fun example2(): Unit = runBlocking {
+        val job = launch(start = CoroutineStart.LAZY) {
+            printWithThread("Hello Launch")
+        }
+
+        delay(1_000L)
+
+        job.start()
     }
 
     suspend fun newRoutine() {
@@ -28,5 +53,9 @@ class SelsApplicationTests {
         val num2 = 2
         yield()
         println(num1 + num2)
+    }
+
+    fun printWithThread(str: Any) {
+        println("[${Thread.currentThread().name}] $str")
     }
 }

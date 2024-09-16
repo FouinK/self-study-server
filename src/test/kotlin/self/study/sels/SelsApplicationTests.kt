9028043211da0ca2343d675bfc8dc87111d80044
Roy <yyss9103@gmail.com>
2024-09-16 16:51:11 +0900

    코루틴 적용

--- src/test/kotlin/self/study/sels/SelsApplicationTests.kt
+++ src/test/kotlin/self/study/sels/SelsApplicationTests.kt
@@ -1,13 +1,32 @@
 package self.study.sels
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class SelsApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+        main()
+    }
 
+    fun main(): Unit = runBlocking {
+        println("start")
+        launch {
+            newRoutine()
+        }
+        yield()
+        println("end")
+    }
+
+    suspend fun newRoutine() {
+        val num1 = 1
+        val num2 = 2
+        yield()
+        println(num1 + num2)
+    }
 }

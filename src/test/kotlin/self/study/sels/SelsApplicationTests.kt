27b7c8bfb2b65c9c84fde55c513273670cf5010a
Roy <yyss9103@gmail.com>
2024-09-18 16:20:26 +0900

    코루틴 async await 처리해서 동시에 기다리는지 확인해보기

--- src/test/kotlin/self/study/sels/SelsApplicationTests.kt
+++ src/test/kotlin/self/study/sels/SelsApplicationTests.kt
@@ -3,6 +3,7 @@ package self.study.sels
 import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.system.measureNanoTime
 
 @SpringBootTest
 class SelsApplicationTests {
@@ -13,6 +14,27 @@ class SelsApplicationTests {
     }
 
     fun main(): Unit = runBlocking {
+        val time = measureNanoTime {
+            // 콜백을 사용하지 않고 동기방식의 코드를 작성 가능 (실제로는 비동기(?))
+            val job1 = async { apiCall1() }
+            val job2 = async { apiCall2() }
+            printWithThread(job1.await() + job2.await())
+        }
+
+        printWithThread("소요시간 : $time ms")
+    }
+
+    suspend fun apiCall1(): Int {
+        delay(1000)
+        return 1
+    }
+
+    suspend fun apiCall2(): Int {
+        delay(1000)
+        return 2
+    }
+
+    fun example5(): Unit = runBlocking {
         // async는 launch와 다르게 결과를 반환할 수 있음
         val job = async {
             3 + 5

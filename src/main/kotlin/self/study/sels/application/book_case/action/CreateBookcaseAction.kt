0ac0a0acaa1fcd7be854884603daaee0ccb49f97
Fouink <yyss9103@gmail.com>
2025-03-07 01:17:22 +0900

    refactor : bookcase 펙토리 메서드 패턴

--- src/main/kotlin/self/study/sels/application/book_case/action/CreateBookcaseAction.kt
+++ src/main/kotlin/self/study/sels/application/book_case/action/CreateBookcaseAction.kt
@@ -3,11 +3,13 @@ package self.study.sels.application.book_case.action
 import self.study.sels.annotation.Action
 import self.study.sels.application.book_case.port.`in`.CreateBookcaseCommand
 import self.study.sels.application.book_case.port.`in`.CreateBookcaseUseCase
+import self.study.sels.model.book_case.BookcaseFactory
 import self.study.sels.model.book_case.BookcaseRepository
 
 @Action
 class CreateBookcaseAction(
     private val bookcaseRepository: BookcaseRepository,
+    private val bookcaseFactory: BookcaseFactory,
 ) : CreateBookcaseUseCase {
     override fun create(
         command: CreateBookcaseCommand
@@ -16,10 +18,15 @@ class CreateBookcaseAction(
             throw Exception("이미 사용중인 이름입니다.")
         }
 
-        val bookCase = command.toEntity()
+        val bookcase = bookcaseRepository.save(
+            bookcaseFactory.create(
+                BookcaseFactory.Command(
+                    name = command.name,
+                    memberId = command.memberId,
+                ),
+            ),
+        )
 
-        val newBookcase = bookcaseRepository.save(bookCase)
-
-        return newBookcase.id
+        return bookcase.id
     }
 }

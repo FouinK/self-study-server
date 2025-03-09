03d19663e71b5bd43b15201b425ac9537850f389
Fouink <yyss9103@gmail.com>
2025-03-10 01:32:50 +0900

    refactor : bookcase, question command usecase 안으로 넣기

--- src/main/kotlin/self/study/sels/application/bookcase/port/in/UpdateBookcaseUseCase.kt
+++ src/main/kotlin/self/study/sels/application/bookcase/port/in/UpdateBookcaseUseCase.kt
@@ -3,3 +3,9 @@ package self.study.sels.application.bookcase.port.`in`
 interface UpdateBookcaseUseCase {
     fun update(command: UpdateBookcaseCommand): String
 }
+
+class UpdateBookcaseCommand(
+    val bookcaseId: Int,
+    val name: String,
+    val memberId: Int,
+)

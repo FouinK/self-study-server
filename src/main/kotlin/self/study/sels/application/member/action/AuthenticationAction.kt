f4c1adb8534c85c3bd56b5a660a9757558dd59d1
Fouink <yyss9103@gmail.com>
2025-04-03 01:56:09 +0900

    feat : 로그인이랑 회원가입 API가 합쳐져서 인증번호 보내는 로직에서 중복 phone 검사 로직 제거

--- src/main/kotlin/self/study/sels/application/member/action/AuthenticationAction.kt
+++ src/main/kotlin/self/study/sels/application/member/action/AuthenticationAction.kt
@@ -4,19 +4,13 @@ import self.study.sels.annotation.Action
 import self.study.sels.application.member.port.`in`.AuthenticationUseCase
 import self.study.sels.controller.dto.AuthenticationMemberRequestDto
 import self.study.sels.model.member.MemberAuthenticationRedisRepository
-import self.study.sels.model.member.MemberRepository
 import self.study.sels.util.AuthCodeUtil
 
 @Action
 class AuthenticationAction(
     private val memberAuthenticationRedisRepository: MemberAuthenticationRedisRepository,
-    private val memberRepository: MemberRepository,
 ) : AuthenticationUseCase {
     override fun execute(command: AuthenticationMemberRequestDto): String {
-        if (memberRepository.existsByPhone(command.phone)) {
-            throw Exception("이미 존재하는 회원 전화번호 입니다.")
-        }
-
         val incrementMemberAuthenticationRequestCount =
             memberAuthenticationRedisRepository.incrementMemberAuthenticationRequestCount(command.phone)
 

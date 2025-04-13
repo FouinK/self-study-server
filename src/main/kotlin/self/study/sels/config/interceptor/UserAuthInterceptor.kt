fce0bb8cf754a35a56176bcebbde347b83d7ec22
tulee <tulee@12cm.co.kr>
2025-04-13 16:33:24 +0900

    chore : 토큰 검증 slice -> removePrefix

--- src/main/kotlin/self/study/sels/config/interceptor/UserAuthInterceptor.kt
+++ src/main/kotlin/self/study/sels/config/interceptor/UserAuthInterceptor.kt
@@ -35,7 +35,7 @@ class UserAuthInterceptor(
         if (authToken.isNullOrEmpty()) {
             throw Exception() // TODO : 여기 kona에 있는거 그대로 정의하기
         }
-        val member: Member = memberRepository.findByAuthToken(authToken.split(" ")[1])
+        val member: Member = memberRepository.findByAuthToken(authToken.removePrefix("bearer "))
             ?: throw Exception() // TODO : 여기 kona에 있는거 그대로 정의하기
 
         memberInfo.init(
